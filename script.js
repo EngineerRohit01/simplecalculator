93ddc7f3a1a46e195c95d510e1a07d3a82c6b7bf
Rohit <rohitkusv@gmail.com>
2024-07-26 20:36:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(){
+		a1 	 = 	parseInt(document.getElementById('a').value)
+		b1 	 = 	parseInt(document.getElementById('b').value)
+		show = 	parseInt(document.getElementById('screen').value)
+		sum  =   a1+b1
+		document.getElementById('screen').value = sum
+	};
+
+	function mul(){
+		a1 	 = 	parseInt(document.getElementById('a').value)
+		b1 	 = 	parseInt(document.getElementById('b').value)
+		show = 	parseInt(document.getElementById('screen').value)
+		mul  =   a1*b1
+		document.getElementById('screen').value = mul
+	};
+
+	function sub(){
+		a1 	 = 	parseInt(document.getElementById('a').value)
+		b1 	 = 	parseInt(document.getElementById('b').value)
+		show = 	parseInt(document.getElementById('screen').value)
+		sub  =   a1-b1
+		document.getElementById('screen').value = sub
+	};
+
+	function div(){
+		a1 	 = 	parseInt(document.getElementById('a').value)
+		b1 	 = 	parseInt(document.getElementById('b').value)
+		show = 	parseInt(document.getElementById('screen').value)
+		div  =   a1/b1
+		document.getElementById('screen').value = div
+	};
